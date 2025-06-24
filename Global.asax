b7d3c951c3ecda4d4be709e10f751d14a44aa930
Mehmet Emin Bozdoğan <23010903051@subu.edu.tr>
2025-06-24 17:48:46 +0300

    Add files via upload

---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="KendimDeneme.MvcApplication" Language="C#" %>
---
+++ Global.asax.cs
@@ -0,0 +1,33 @@
+﻿using System.Globalization;
+using System.Threading;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+
+namespace KendimDeneme
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+
+        protected void Application_BeginRequest()
+        {
+            var cultureInfo = new CultureInfo("tr-TR");
+
+            if (Request.Cookies["lang"] != null)
+            {
+                var lang = Request.Cookies["lang"].Value;
+                cultureInfo = new CultureInfo(lang);
+            }
+
+            Thread.CurrentThread.CurrentCulture = cultureInfo;
+            Thread.CurrentThread.CurrentUICulture = cultureInfo;
+        }
+    }
+}
